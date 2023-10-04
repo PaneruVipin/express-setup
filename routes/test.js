d0a5a815ebe774e73fca44877bf7ac1bc7e6a8e4
PaneruVipin <paneruvipinbusiness@gmail.com>
2023-10-04 20:23:23 +0530

    miner

--- routes/test.js
+++ routes/test.js
@@ -1,3 +1,14 @@
-appRouter.get("/test", async function () {
+const unprocessableException = require("../exceptions/unprocessableException");
+const Env = require("../lib/env");
+
+appRouter.get("/test1", async function () {
   respond("respond with a resource");
+});
+
+appRouter.get("/test2", async function () {
+  unprocessableException("an error occured", 401);
+});
+
+appRouter.get("/test3", async function () {
+  respond("Env testing: " + Env.APP_NAME);
 });
